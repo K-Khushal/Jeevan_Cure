c44a28ab96318a3967dfa0977275a7f592b1bb3d
Khushal <khandelwalkhushal79@gmail.com>
2024-11-15 00:52:54 +0530

    added base url

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,31 +1,34 @@
-import {adminClient, multiSessionClient, organizationClient, passkeyClient, twoFactorClient, usernameClient} from "better-auth/client/plugins";
-import {createAuthClient} from "better-auth/react";
-import {toast} from "sonner";
+import {
+  adminClient,
+  multiSessionClient,
+  organizationClient,
+  passkeyClient,
+  twoFactorClient,
+  usernameClient,
+} from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
+import { toast } from "sonner";
 
 export const client = createAuthClient({
-	plugins: [
-		organizationClient(),
-		twoFactorClient({
-			redirect: true,
-			twoFactorPage: "/two-factor",
-		}),
-		passkeyClient(),
-		adminClient(),
-		multiSessionClient(),
-		usernameClient(),
-	],
-	fetchOptions: {
-		onError(e) {
-			if (e.error.status === 429) {
-				toast.error("Too many requests. Please try again later.");
-			}
-		},
-	},
+  baseURL: "http://localhost:3000",
+  plugins: [
+    organizationClient(),
+    twoFactorClient({
+      redirect: true,
+      twoFactorPage: "/two-factor",
+    }),
+    passkeyClient(),
+    adminClient(),
+    multiSessionClient(),
+    usernameClient(),
+  ],
+  fetchOptions: {
+    onError(e) {
+      if (e.error.status === 429) {
+        toast.error("Too many requests. Please try again later.");
+      }
+    },
+  },
 });
 
-export const {
-	signUp,
-	signIn,
-	signOut,
-	useSession,
-} = client;
+export const { signUp, signIn, signOut, useSession } = client;
