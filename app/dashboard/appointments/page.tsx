d8a0ac626bde472941dace5ec820436445ccba7b
Khushal <khandelwalkhushal79@gmail.com>
2024-11-19 01:42:30 +0530

    sidebar refactor

---
+++ app/dashboard/appointments/page.tsx
@@ -0,0 +1,7 @@
+export default function AppointmentsPage() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 pt-0 min-h-[100vh]">
+      <h1>Appointments</h1>
+    </div>
+  );
+}
---
+++ app/dashboard/documents/page.tsx
@@ -0,0 +1,7 @@
+export default function DocumentsPage() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 pt-0 min-h-[100vh]">
+      <h1>Documents</h1>
+    </div>
+  );
+}
