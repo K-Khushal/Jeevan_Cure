106945cdf9fdfb1e7915d60fae5955089d25d8ab
Khushal <khandelwalkhushal79@gmail.com>
2024-11-24 02:45:29 +0530

    added session to dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { AppSidebar } from "@/components/dashboard/app-sidebar";
 import { Breadcrumb, BreadcrumbItem } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
