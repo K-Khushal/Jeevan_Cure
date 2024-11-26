5c627f86208fe58380c733f4cd0cd46c98abe3df
Khushal <khandelwalkhushal79@gmail.com>
2024-11-27 01:47:53 +0530

    updated middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -16,31 +16,18 @@ export default async function authMiddleware(request: NextRequest) {
 
   const authPages = ["/sign-in", "/sign-up"];
   const isAuthPage = authPages.includes(request.nextUrl.pathname);
-  const privatePages = [
-    "/dashboard",
-    "/user-profile",
-    "/dashboard/appointment",
-  ];
-  const isPrivatePage = privatePages.includes(request.nextUrl.pathname);
 
-  // If user is not logged in and tries to access any protected route
-  // if (!session) {
-  //   return NextResponse.redirect(new URL('/sign-in', request.url));
-  // }
-
-  // If logged-in user tries to access login/signup pages
-  if (isAuthPage && session) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+  if (!session && !isAuthPage) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
-  // If non-logged-in user tries to access dashboard
-  if (isPrivatePage && !session) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+  if (session && isAuthPage) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/user-profile", "/sign-in", "/sign-up"],
+  matcher: ["/dashboard/:path*", "/sign-in", "/sign-up"],
 };
