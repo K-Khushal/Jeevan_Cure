9fd11858d075ca8a9f76a9da154798ab3bb1620b
Khushal <khandelwalkhushal79@gmail.com>
2024-11-27 23:23:49 +0530

    /user-profile fix

--- app/dashboard/user-profile/page.tsx
+++ app/dashboard/user-profile/page.tsx
@@ -1,19 +1,41 @@
+"use client";
+
 import { getActiveSessions } from "@/actions/get-active-session";
 import Loading from "@/app/dashboard/loading";
-import { redirect } from "next/navigation";
-import { Suspense } from "react";
+import { Session } from "@/lib/auth-types";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import UserCard from "./user-card";
 
-export default async function UserProfilePage() {
-  const activeSessions = await getActiveSessions().catch((e) => {
-    throw redirect("/sign-in");
-  });
+export default function UserProfilePage() {
+  const [activeSessions, setActiveSessions] = useState<Session["session"][]>(
+    [],
+  );
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchActiveSessions = async () => {
+      try {
+        const sessions = await getActiveSessions();
+        setActiveSessions(sessions);
+      } catch (e) {
+        router.push("/sign-in");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchActiveSessions();
+  }, [router]);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <Suspense fallback={<Loading />}>
-        <UserCard activeSessions={activeSessions} />
-      </Suspense>
+      {activeSessions ? <UserCard activeSessions={activeSessions} /> : null}
     </div>
   );
 }
